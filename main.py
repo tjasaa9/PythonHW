64f2951e5fd71b2fbe3214e7dcb86fcbc6086175
tjasaa9 <tjasa9trost9@gmail.com>
2021-07-28 17:25:44 +0200

    added json files

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import json
+
+with open("results_file.json", "r") as results:
+    results = json.loads(results_file.read())
+    print("Top results: " + str(results))
+
 user_name = input("Dobrodošel/a v kvizu! Za začetek mi zaupaj tvoje ime: ")
 print(f"Pozdravljen/a, {user_name} :). To je kviz, na katerega odgovarjaš le z eno (začetno) črko. Primer: Na katero črko se začne beseda 'pes'? Odgovor: p.  Razumljivo? Pa začniva!")
 
